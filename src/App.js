3d9b9ea93f62074331667f9c5c4259775b8ad179
Chirag Saini <chiragsaini7777@gmail.com>
2020-07-14 11:39:58 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
+import Helmet from "react-helmet";
+import favicon from './favicon.ico';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Helmet>
+        <title>Netflix Clone</title>
+        <meta 
+        name="description"
+        content="Netflix Clone created using ReactJS and Firebase, TMDB API"
+        />
+        <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
+      </Helmet>
+      <Nav />
+      <Banner />
+      <Row title="NETFLIX Originals" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorroMovies} />
+      <Row title="Romantic Movies" fetchUrl={requests.fetchRomanticMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+    const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(fetchUrl);
+                setMovies(res.data.results);
+                return res;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchData();
+    }, [fetchUrl]);
+    
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        }
+    }
+
+    const handleClick = (movie) => {
+        if (trailerUrl){
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.name || "")
+            .then(url => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get("v"));
+            })
+            .catch(error => console.log(error))
+        }
+    }
+
+    return (
+        <div className="row" >
+            <h2>{title}</h2>
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img
+                        onClick={() => handleClick(movie)}
+                        key={movie.id}
+                        src={`${baseUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                        alt={movie.name}
+                        className={`row__poster ${isLargeRow && 'row__poster__large'}`}
+                    />
+                ))}
+            </div>
+            <div>
+                {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+            </div>
+        </div>
+    );
+}
+
+export default Row;
